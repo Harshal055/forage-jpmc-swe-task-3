829380543c7c941f9a649b745020102df9dbf38a
Harshal055 <119702320+Harshal055@users.noreply.github.com>
2024-06-22 16:09:35 +0530

    TASK 3 add

--- datafeed/server3.py
+++ datafeed/server3.py
@@ -152,7 +152,7 @@ def generate_csv():
 
 def read_csv():
     """ Read a CSV or order history into a list. """
-    with open('test.csv', 'rt') as f:
+    with open('forage-jpmc-swe-task-3/test.csv', 'rt') as f:
         for time, stock, side, order, size in csv.reader(f):
             yield dateutil.parser.parse(time), stock, side, float(order), int(size)
 
